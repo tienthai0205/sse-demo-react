9ba2e81368a30ee5fd53677a6eddbf3f6662087c
tien <460592@student.saxion.nl>
2022-04-04 14:12:11 +0200

    update README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Table } from 'antd';
+import 'antd/dist/antd.css';
+
+const BaseURL = "http://localhost:3000";
 
 function App() {
+  const [events, setEvents] = useState([]);
+
+  const updateEvents = (data) => {
+    const parsedData = JSON.parse(data);
+    setEvents((events) => {
+      if(!Array.isArray(parsedData)){
+        return [...events, parsedData]
+      } else {
+        return parsedData
+      }
+    }
+    );
+  };
+
+  useEffect(() => {
+    const eventSource = new EventSource(`${BaseURL}/events`);
+    eventSource.onmessage = (e) => updateEvents(e.data);
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+  const columns = [
+	{
+	  title: 'ID',
+	  dataIndex: 'id',
+	  key: 'id',
+	},
+	{
+	  title: 'Info',
+	  dataIndex: 'info',
+	  key: 'info',
+	},
+	{
+	  title: 'Source',
+	  dataIndex: 'source',
+	  key: 'source',
+	},
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="container">
+		<h1>List of facts and sources</h1>
+      <Table columns={columns} dataSource={events} />
     </div>
   );
 }
